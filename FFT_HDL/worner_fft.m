27e66ec946982dcb86764aa71303125b393142e4
Jake <jake.macneal@gmail.com>
2017-03-21 17:00:36 -0400

    Worner FFT/IFFT (working), fft_hdl project file

---
+++ FFT_HDL/worner_fft.m
@@ -0,0 +1,20 @@
+function y = worner_fft(x)
+    N= length(x); 
+    x = bitrevorder(x);
+    q = log2(N);
+
+    for j = 1:q
+        m = 2^(j-1);
+        d = exp(-pi*1i/m).^(0:m-1);
+        for k = 1:2^(q-j)
+              s = 2*(k-1)*m+1;   % start-index
+              e = 2*k*m;             % end-index
+              r = s + (e-s+1)/2;  % middle-index
+              y_top       = x(s:r-1);
+              y_bottom = x(r:e);
+              z = d .* y_bottom;
+              y = [y_top + z, y_top - z];
+              x(s:e) = y;
+        end
+    end
+end
---
+++ FFT_HDL/worner_ifft.m
@@ -0,0 +1,20 @@
+function y = worner_ifft(x)
+    n = length(x);
+    x = bitrevorder(x);
+    q = round(log(n)/log(2));
+    for j = 1:q
+        m = 2^(j-1);
+        d = exp(pi * 1i /m).^(0:m-1);
+        k = 1:2^(q-j);
+        for k =  1:2^(q-j)
+            s = (k-1)*2*m+1; % start-index
+            e = k*2*m; % end-index
+            r = s + (e-s+1)/2; % middle-index
+            y_top = x(s:(r-1));
+            y_bottom = x(r:e);
+            z = d .* y_bottom;
+            y = [y_top + z, y_top - z];
+            x(s:e) = y;
+        end
+    end
+end
